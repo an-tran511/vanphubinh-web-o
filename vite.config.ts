64a73bc28fbbf075afff968ba70b4a271f747dee
An <an.tran511@outlook.com>
2023-12-25 20:34:37 +0700

    downgrade to vite 4

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,9 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tsconfigPaths from 'vite-tsconfig-paths';
-import legacy from '@vitejs/plugin-legacy';
+import { ViteAliases } from 'vite-aliases';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    tsconfigPaths(),
-    legacy({
-      targets: ['defaults', 'not IE 11'],
-    }),
-  ],
+  plugins: [react(), tsconfigPaths(), ViteAliases({ prefix: '@' })],
 });
