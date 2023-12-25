a4db69eddd2947b4ee42140c59b75b9f665fad82
An <an.tran511@outlook.com>
2023-12-25 20:34:16 +0700

    update config for app

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import tsconfigPaths from 'vite-tsconfig-paths';
+import legacy from '@vitejs/plugin-legacy';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    tsconfigPaths(),
+    legacy({
+      targets: ['defaults', 'not IE 11'],
+    }),
+  ],
+});
